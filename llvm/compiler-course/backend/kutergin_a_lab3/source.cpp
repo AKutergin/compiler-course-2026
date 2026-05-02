16a34d45d7afd04af3ddab8b9607c7a4060bdb83
AKutergin <epta337@gmail.com>
2026-05-02 18:40:39 +0300

    cf fix

--- llvm/compiler-course/backend/kutergin_a_lab3/source.cpp
+++ llvm/compiler-course/backend/kutergin_a_lab3/source.cpp
@@ -21,16 +21,17 @@ public:
     bool Changed = false;
 
     for (auto &MBB : MF) {
-      for (auto I = MBB.begin(); I != MBB.end(); ) {
+      for (auto I = MBB.begin(); I != MBB.end();) {
         MachineInstr &MI = *I;
         unsigned Opcode = MI.getOpcode();
 
         if (Opcode == X86::INC32r || Opcode == X86::INC64r ||
             Opcode == X86::DEC32r || Opcode == X86::DEC64r) {
-          
+
           Register Reg = MI.getOperand(0).getReg();
-          int Accumulator = (Opcode == X86::INC32r || Opcode == X86::INC64r) ? 1 : -1;
-          
+          int Accumulator =
+              (Opcode == X86::INC32r || Opcode == X86::INC64r) ? 1 : -1;
+
           auto NextI = std::next(I);
           SmallVector<MachineInstr *, 4> ToErase;
           ToErase.push_back(&MI);
@@ -40,8 +41,9 @@ public:
             if ((NextOp == X86::INC32r || NextOp == X86::INC64r ||
                  NextOp == X86::DEC32r || NextOp == X86::DEC64r) &&
                 NextI->getOperand(0).getReg() == Reg) {
-              
-              Accumulator += (NextOp == X86::INC32r || NextOp == X86::INC64r) ? 1 : -1;
+
+              Accumulator +=
+                  (NextOp == X86::INC32r || NextOp == X86::INC64r) ? 1 : -1;
               ToErase.push_back(&*NextI);
               ++NextI;
             } else {
@@ -63,11 +65,13 @@ public:
                 .addReg(Reg)
                 .addImm(Accumulator);
 
-            for (auto *Inst : ToErase) Inst->eraseFromParent();
+            for (auto *Inst : ToErase)
+              Inst->eraseFromParent();
             I = NextI;
             Changed = true;
           } else {
-            for (auto *Inst : ToErase) Inst->eraseFromParent();
+            for (auto *Inst : ToErase)
+              Inst->eraseFromParent();
             I = NextI;
             Changed = true;
           }
@@ -87,4 +91,6 @@ public:
 
 char IncDecToAddSubPass::ID = 0;
 
-static RegisterPass<IncDecToAddSubPass> X("inc-dec-to-add-sub", "X86 INC/DEC to ADD/SUB transformation", false, false);
+static RegisterPass<IncDecToAddSubPass>
+    X("inc-dec-to-add-sub", "X86 INC/DEC to ADD/SUB transformation", false,
+      false);
